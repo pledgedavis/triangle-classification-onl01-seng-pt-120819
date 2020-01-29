7551b844e5c47a001037f62d6cd27f338abbce2e
Pledge Davis <pledgedavis14@gmail.com>
2020-01-29 23:45:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -31,11 +31,11 @@ class Triangle
     # if @side_1 <= 0  || @side_2 <= 0 || @side_3 <= 0
     #     raise TriangleError
       [@side_1, @side_2, @side_3].each do |side|
-        if side <= 0
-          triangle << false
-        end
-        raise TriangleError if triangle.include?(false)
-     end
+    #     if side <= 0
+    #       triangle << false
+    #     end
+    #     raise TriangleError if triangle.include?(false)
+    #  end
     end
 
     class TriangleError < StandardError
