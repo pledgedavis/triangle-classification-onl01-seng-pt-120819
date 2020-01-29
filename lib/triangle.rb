958c9e01c4a054e256c5b8fceacde23ee3840086
Pledge Davis <pledgedavis14@gmail.com>
2020-01-29 23:43:03 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -46,15 +46,4 @@ class Triangle
 end
 
 
-# def get_married(person)
-#     self.partner = person
-#     if person.class != Person
-#       begin
-#         raise PartnerError
-#       rescue PartnerError => error
-#           puts error.message
-#       end
-#     else
-#       person.partner = self
-#     end
-# end
+#
